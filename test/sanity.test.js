f5c745b09d11b64729e9a3320b360080a7f0f733
Mike McNeil <git+mikermcneil@sailsjs.com>
2018-11-19 02:41:48 -0600

    Add tests for sum and avg (but the avg one still doesn't really work)

--- test/sanity.test.js
+++ test/sanity.test.js
@@ -42,7 +42,7 @@ describe('sanity', ()=>{
       await adapter.ƒ.getConnection(mgr);
       await adapter.ƒ.releaseConnection(firstConnection);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
     it('should support querying', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db = (await adapter.ƒ.getConnection(mgr)).connection;
@@ -55,7 +55,7 @@ describe('sanity', ()=>{
       assert(queryFailureErr);
       assert.equal('noSuchPhysicalModel', (await adapter.ƒ.parseNativeQueryError(queryFailureErr)).footprint.identity);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
     it('should support transactions', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db1 = (await adapter.ƒ.getConnection(mgr)).connection;
@@ -67,7 +67,7 @@ describe('sanity', ()=>{
       await adapter.ƒ.sendNativeQuery(db2, 'SELECT * FROM notarealtable').tolerate('queryFailed');
       await adapter.ƒ.rollbackTransaction(db2);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
     it('should support auto-migrations', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db = (await adapter.ƒ.getConnection(mgr)).connection;
@@ -79,7 +79,7 @@ describe('sanity', ()=>{
       ]);
       await adapter.ƒ.setPhysicalSequence(db, 'the_foo_id_seq', 1000);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
     it('should support inserting a record (+"fetch")', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db = (await adapter.ƒ.getConnection(mgr)).connection;
@@ -103,7 +103,7 @@ describe('sanity', ()=>{
       assert(secondResult);
       assert.equal(secondResult.the_beep, secondBeep);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
     it('should support batch inserting many records (+"fetch")', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db = (await adapter.ƒ.getConnection(mgr)).connection;
@@ -149,16 +149,11 @@ describe('sanity', ()=>{
       assert(secondResult);
       assert.equal(secondResult[1].the_beep, eighthBeep);
       await adapter.ƒ.destroyManager(mgr);
-    });
-    it('should support doing a simple count', async()=>{
+    });//</it>
+    it('should support running two count queries, with consistent results', async()=>{
       var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
       var db = (await adapter.ƒ.getConnection(mgr)).connection;
-      var total = await adapter.ƒ.countRecords({
-        method: 'count',
-        using: 'the_foo',
-        where: {}
-      }, db, DRY_ORM);
-      assert(total);
+      var total = await adapter.ƒ.countRecords({ method: 'count', using: 'the_foo', where: {} }, db, DRY_ORM);
       assert(typeof total === 'number');
       await adapter.ƒ.createRecord({
         method: 'create',
@@ -167,15 +162,50 @@ describe('sanity', ()=>{
           the_beep: Date.now()+Math.random()//eslint-disable-line camelcase
         }
       }, db, DRY_ORM);
-      var newTotal = await adapter.ƒ.countRecords({
-        method: 'count',
-        using: 'the_foo',
-        where: {}
-      }, db, DRY_ORM);
-      assert(newTotal);
+      var newTotal = await adapter.ƒ.countRecords({ method: 'count', using: 'the_foo', where: {} }, db, DRY_ORM);
       assert(typeof newTotal === 'number');
       assert.equal(newTotal, total + 1);
       await adapter.ƒ.destroyManager(mgr);
-    });
+    });//</it>
+    it('should support running two sum queries, with consistent results', async()=>{
+      var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
+      var db = (await adapter.ƒ.getConnection(mgr)).connection;
+      var sumTotal = await adapter.ƒ.sumRecords({ method: 'sum', using: 'the_foo', numericAttrName: 'the_beep', where: {} }, db, DRY_ORM);
+      assert(typeof sumTotal === 'number');
+      var amountToAdd = Date.now()+Math.random();
+      await adapter.ƒ.createRecord({
+        method: 'create',
+        using: 'the_foo',
+        newRecord: {
+          the_beep: amountToAdd//eslint-disable-line camelcase
+        }
+      }, db, DRY_ORM);
+      var newSumTotal = await adapter.ƒ.sumRecords({ method: 'sum', using: 'the_foo', numericAttrName: 'the_beep', where: {} }, db, DRY_ORM);
+      assert(typeof newSumTotal === 'number');
+      assert(newSumTotal === sumTotal + amountToAdd);
+      await adapter.ƒ.destroyManager(mgr);
+    });//</it>
+    it('should support running two avg queries, with consistent results', async()=>{
+      var mgr = (await adapter.ƒ.createManager(dbUrl)).manager;
+      var db = (await adapter.ƒ.getConnection(mgr)).connection;
+      var firstAvg = await adapter.ƒ.avgRecords({ method: 'avg', using: 'the_foo', numericAttrName: 'the_beep', where: {} }, db, DRY_ORM);
+      assert(typeof firstAvg === 'number');
+      var originalNumRecords = await adapter.ƒ.countRecords({ method: 'count', using: 'the_foo', where: {} }, db, DRY_ORM);
+      var valInNewRecord = Date.now()+Math.random();
+      await adapter.ƒ.createRecord({
+        method: 'create',
+        using: 'the_foo',
+        newRecord: {
+          the_beep: valInNewRecord//eslint-disable-line camelcase
+        }
+      }, db, DRY_ORM);
+      var secondAvg = await adapter.ƒ.avgRecords({ method: 'avg', using: 'the_foo', numericAttrName: 'the_beep', where: {} }, db, DRY_ORM);
+      assert(typeof secondAvg === 'number');
+      var expectedDelta = valInNewRecord/(originalNumRecords+1);
+      assert.equal(secondAvg, valInNewRecord > firstAvg  ? (firstAvg+expectedDelta)  : (firstAvg-expectedDelta));
+      // ^^e.g. consider a case where original avg is 2, and there are 3 records (with values 1, 2, and 3)
+      // If a new record w/ value 1 is added, then the new average becomes 1.75 (i.e. subtract 1/4)
+      await adapter.ƒ.destroyManager(mgr);
+    });//</it>
   }//∞
 });//∂
